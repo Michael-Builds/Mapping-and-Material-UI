12cdea5802c1f2cd8eb55c44f6f5e33ad3c74001
Kabanda Kpanti Michael <you@example.com>
2023-03-29 12:28:57 +0000

    Mapping and Material UI in React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { useState } from "react";
+import SendIcon from '@mui/icons-material/Send';
+
+
+const Array = [
+{
+  name: "Michael",
+  Expertise: "FrontEnd Developer",
+  Experience :"2 Years"
+},
+{
+  name: "Julius",
+  Expertise: "BackEnd Developer",
+  Experience :"3 Years"
+},
+{
+  name: "Prince",
+  Expertise: "FullStack Developer",
+  Experience :"6 Years"
+},
+
+];
 
 function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleClick = () => {
+
+    setCurrentIndex((currentIndex + 1) % Array.length);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <h1>Personal Information</h1>
+   {Array.map((person, index) => (
+        <div key={index} style={{ display: index === currentIndex ? "block" : "none" }}
+        className={`person-info ${index === currentIndex ? "active" : ""}`}>
+          <h2>
+            Name: {person.name}
+            <br />
+            Expertise: {person.Expertise}
+            <br />
+            Experience: {person.Experience}
+          </h2>
+        </div>
+      ))}
+       <button onClick={handleClick}> <SendIcon /> </button>
+   </>
   );
 }
 
